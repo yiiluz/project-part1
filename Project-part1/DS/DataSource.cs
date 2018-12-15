f4d6ae4a733fc3d8becf40268d02528e91c536f4
shlomi perry <SlomiPerry@gmail.com>
2018-12-16 01:19:17 +0200

    I DO NOT REMMBER

--- Project-part1/DS/DataSource.cs
+++ Project-part1/DS/DataSource.cs
@@ -3,22 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ex1_BE;
 namespace DS
 {
     public class DataSource
-    {
-        protected static DataSource dataSource = null;
-        public static List<Ex1_BE.Test> tests;
-        public static List<Ex1_BE.Tester> testers;
-        public static List<Ex1_BE.Trainee> trainees;
-        public static DataSource getDataSource()
-        {
-            if (dataSource==null)
-            {
-                dataSource = new DataSource();
-                return dataSource;
-            }
-            return dataSource;
-        }
+    {    
+        public static List<Test> tests;
+        public static List<Tester> testers;
+        public static List<Trainee> trainees;
     }
 }
