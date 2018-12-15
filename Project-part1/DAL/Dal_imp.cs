08db455401634511354ae09f0528fcd0a15401d4
shlomi perry <SlomiPerry@gmail.com>
2018-12-16 00:07:07 +0200

    IDAL factory

---
+++ Project-part1/DAL/Dal_imp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex1_BE;
+using DS;
+namespace DAL
+{
+    class Dal_imp : IDAL
+    {
+        protected Dal_imp() { }
+        protected static Dal_imp instance = null;
+        public static Dal_imp GetInstance()
+        {
+            if (instance==null)
+            {
+                instance = new Dal_imp();            
+            }
+            return instance;
+        }
+
+        void IDAL.AddTest(Test t)
+        {
+            DataSource dataSource 
+        }
+
+        void IDAL.AddTester(Tester T)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDAL.AddTrainee(Trainee T)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Tester> IDAL.GetTestersList()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Test> IDAL.GetTestsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Trainee> IDAL.GetTraineeList()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDAL.RemoveTester(Tester T)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDAL.RemoveTrainee(Trainee T)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDAL.UpdateTest(Test t)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDAL.UpdateTesterDetails(Tester T)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDAL.UpdateTraineeDetails(Trainee T)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Project-part1/DAL/Idal.cs
+++ Project-part1/DAL/Idal.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Ex1_BE;
 namespace DAL
 {  
     interface IDAL
     {
-        void addTest(Object T);
-        void removeTest(Object T);
-        void updateTestDetails(Object T);
-        void addStudent(Object T);
-        void removeStudent(Object T);
-        void updateStudentDetails(Object T);
-        Object getListOfAllExaminers();
-        Object getListAllStudents();
-        Object gatListAllTests();
+        void AddTester(Tester T);
+        void RemoveTester(Tester T);
+        void UpdateTesterDetails(Tester T);
+        void AddTrainee(Trainee T);
+        void RemoveTrainee(Trainee T);
+        void UpdateTraineeDetails(Trainee T);
+        void AddTest(Test t);
+        void UpdateTest(Test t);
+        List<Tester> GetTestersList();
+        List<Trainee> GetTraineeList();
+        List<Test> GetTestsList();
     }
 }
