84f9a21f8d0bc1236fd62d18920fa19834a025f7
shlomi perry <SlomiPerry@gmail.com>
2018-12-19 16:35:27 +0200

    WFREFDSDSFD

--- Project-part1/Ex1_BE/Enums.cs
+++ Project-part1/Ex1_BE/Enums.cs
@@ -9,4 +9,6 @@ namespace Ex1_BE
     public enum GenderEnum { Male = 1, Female = 2 }
     public enum CarTypeEnum { MotorCycle = 1, PrivateCar, Truck12Tons, TruckUnlimited, Bus }
     public enum GearboxTypeEnum { auto = 1, manual = 2 }
+
+
 }
